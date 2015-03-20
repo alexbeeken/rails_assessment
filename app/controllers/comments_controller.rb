659a6ceb5342285eced575bf6fe5f2cd5c95d8c0
Alex Beeken <aeb242@gmail.com>
2015-03-20 14:03:21 -0700

    Edit and Delete Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,7 +20,28 @@ class CommentsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
-    @comment = Comment.find(params[:comment_id])
+    @comment = Comment.find(params[:post_id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      @post.comments << @comment
+      flash[:notice] = "Comment Posted Successfully"
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "There was a problem. Please fix mistakes or contact support"
+      render :new
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @comment = Comment.find(params[:post_id])
+    flash[:notice] = "#{@post.title} successfully deleted."
+    @comment.destroy
+    redirect_to post_path(@post)
   end
 
   private
--- spec/features/add_post_pages_spec.rb
+++ spec/features/add_post_pages_spec.rb
@@ -46,3 +46,34 @@ describe "the add a post comment process" do
     expect(page).to have_content "Successfully"
   end
 end
+
+describe "the edit a post comment process" do
+  it 'edits a comment for a post' do
+    post = Post.create({:title => "How to Be a Dog", :body => "Sniff, Sleep and Slumber"})
+    visit "/posts"
+    click_on 'How to Be a Dog'
+    click_on 'Comment'
+    fill_in 'Name', :with => 'Cat'
+    fill_in 'Body', :with => 'You disgust me'
+    click_on 'comment'
+    click_on 'Edit Comment'
+    fill_in 'Name', :with => 'Dog'
+    fill_in 'Body', :with => 'I like bones'
+    click_on 'comment'
+    expect(page).to have_content "Successfully"
+  end
+end
+
+describe "the delete a post comment process" do
+  it 'delete a comment for a post' do
+    post = Post.create({:title => "How to Be a Dog", :body => "Sniff, Sleep and Slumber"})
+    visit "/posts"
+    click_on 'How to Be a Dog'
+    click_on 'Comment'
+    fill_in 'Name', :with => 'Cat'
+    fill_in 'Body', :with => 'You disgust me'
+    click_on 'comment'
+    click_on 'Delete Comment'
+    expect(page).to have_content "successfully"
+  end
+end
