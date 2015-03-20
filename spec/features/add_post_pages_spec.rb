2c4ad0f3c09ac4dd644bbed325029f9bab389c68
Alex Beeken <aeb242@gmail.com>
2015-03-20 11:50:42 -0700

    Add Comments to Posts

--- spec/features/add_post_pages_spec.rb
+++ spec/features/add_post_pages_spec.rb
@@ -33,3 +33,16 @@ describe "the delete post process" do
     expect(page).to have_content "deleted"
   end
 end
+
+describe "the add a post comment process" do
+  it 'creates a comment for a post' do
+    post = Post.create({:title => "How to Be a Dog", :body => "Sniff, Sleep and Slumber"})
+    visit "/posts"
+    click_on 'How to Be a Dog'
+    click_on 'Comment'
+    fill_in 'Name', :with => 'Cat'
+    fill_in 'Body', :with => 'You disgust me'
+    click_on 'comment'
+    expect(page).to have_content "Successfully"
+  end
+end
